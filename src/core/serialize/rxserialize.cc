382ecaf9147606915d34e5f2e21b327ea3126870
lcmftianci <707010543@qq.com>
2019-08-30 09:02:59 +0800

    update rxserialize

---
+++ src/core/serialize/rxserialize.cc
@@ -0,0 +1,18 @@
+#include "rxserialize.h"
+
+using namespace rxnet;
+using namespace coreutil;
+
+
+rxserialize::rxserialize()
+{
+
+}
+
+rxserialize::~rxserialize()
+{
+
+}
+
+				
+#endif
---
+++ src/core/serialize/rxserialize.h
@@ -0,0 +1,18 @@
+#ifndef _RX_SERIALIZE_H__
+#define _RX_SERIALIZE_H__
+
+namespace rxnet{
+	namespace coreutil{
+		class rxserialize{
+			public:
+				rxserialize();
+				virtual ~rxserialize();
+
+				
+			private:
+				
+		};
+	}
+}
+
+#endif
